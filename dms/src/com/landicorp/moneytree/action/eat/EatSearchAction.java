11c68fb10e491d40783b7766b578929a51ba9568
旭裕 李 <lixuyu0322@126.com>
2016-07-23 13:28:06 +0800

    吃数排序

--- dms/src/com/landicorp/moneytree/action/eat/EatSearchAction.java
+++ dms/src/com/landicorp/moneytree/action/eat/EatSearchAction.java
@@ -1,8 +1,11 @@
 package com.landicorp.moneytree.action.eat;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import com.landicorp.core.action.BaseActionSupport;
+import com.landicorp.moneytree.entities.ChargeRecord;
 import com.landicorp.moneytree.entities.Eat;
 import com.landicorp.moneytree.service.IEatService;
 
@@ -39,6 +42,20 @@ public class EatSearchAction extends BaseActionSupport {
 		eat = new Eat();
 		eat.setUser(getSessionUser());
 		eatList = eatService.getAllEat(eat, getPager());
+		Comparator<Eat> comparator = new Comparator<Eat>() {
+			public int compare(Eat s1, Eat s2) {
+				int num1 = s1.getEatValue();
+				int num2 = s2.getEatValue();
+
+				if (num1 != num2) {
+					return (int) (num1 - num2);
+				} else {
+					return Integer.valueOf(s1.getId()) - Integer.valueOf(s2.getId());
+				}
+			}
+		};
+		Collections.sort(eatList, comparator);
+		
 		return SUCCESS;
 	}
 
